92fff5200b216849a02fd6647597ce3adbb473d3
IvanTyukin <ivantyukin@mail.ru>
2022-05-16 13:04:33 +0300

    Update lab3.js

--- js/lab3/lab3.js
+++ js/lab3/lab3.js
@@ -4,8 +4,8 @@
  * @param {num} число дробную часть которого надо получить
  */
  function getDecimal(num) {
-    var str = "" + num;
-    var zeroPos = str.indexOf(".");
+    let str = "" + num;
+    let zeroPos = str.indexOf(".");
     if (zeroPos == -1) return 0;
     str = str.slice(zeroPos);
     return +str;
