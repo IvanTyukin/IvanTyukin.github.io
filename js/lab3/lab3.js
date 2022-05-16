4f88e8772b87a88a2b581f96f6df4ad86510e15f
IvanTyukin <ivantyukin@mail.ru>
2022-05-16 13:33:21 +0300

    js/lab3/lab3.js

--- js/lab3/lab3.js
+++ js/lab3/lab3.js
@@ -15,3 +15,25 @@
     return str[0].toUpperCase() + str.slice(1);
   }
   
+  function checkSpam(str) {
+    let firstWord = 'viagra';
+    let secondWord = 'xxx';
+    let string = str.toLowerCase();
+    let result;
+
+    if (checkTheWord(firstWord) || checkTheWord(secondWord)) {
+        return true;
+    }
+    return false;
+
+    function checkTheWord(word) {
+        for (let i = 0; i < string.length - 1; i++) {
+            if (string[i] == word[0]) {
+                result = string.slice(i, i + word.length);
+                if (result == word) {
+                    return true;
+                }
+            }
+        }
+    }
+}
