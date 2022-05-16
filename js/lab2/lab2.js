5e5edde87805dd8dd49acaa5b06bd926c8bc42ea
IvanTyukin <ivantyukin@mail.ru>
2022-05-16 14:09:12 +0300

    js/lab2/lab2.js

--- js/lab2/lab2.js
+++ js/lab2/lab2.js
@@ -58,24 +58,17 @@ function factorial(n) {
  * @returns {number} - n-ное число Фибоначи
  * 
  */
-function fib(n) {
-  let count = 0;
-  let n1 = 1,
-      n2 = 1,
-      c = 0;
-  if (n == 0)
-      n2 = 0;
-  else
-  if (n >= 1)
-      if (n >= 2) {
-          for (let i = 2; i < n; i++) {
-              c = n2;
-              n2 = n1 + n2;
-              n1 = c;
-          }
-      }
-  return n2;
-}
+  function fib(n) {
+    let a = 1n;
+    let b = 1n;
+    let c;
+    for (let i = 3; i <= n; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  }
 
 
 /**
