27c7ffc7a4e5a53af94955d9519cc8679471da49
IvanTyukin <ivantyukin@mail.ru>
2022-05-17 13:47:35 +0300

    Update lab3.js

--- js/lab3/lab3.js
+++ js/lab3/lab3.js
@@ -2,7 +2,7 @@
 
  function getDecimal(num) {
             let a = num.slice(num.indexOf('.'));
-            let str = 0 + a;
+            let str = 0 + a; 
             if (Number(num) < 0) {
                 str = 1 - Number(str);
                 str = str.toFixed(a.length - 1);
